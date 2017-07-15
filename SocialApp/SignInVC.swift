68e6c9ab55dbed775e3d0fa78b04598ba49a91e0
Gabriel Trujillo Gómez <gabotrugomez@Gabriels-iMac.local>
2017-07-14 23:47:22 -0500

    First Commit

---
+++ SocialApp/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Gabriel Trujillo Gómez on 7/14/17.
+//  Copyright © 2017 Gabriel Trujillo Gómez. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
